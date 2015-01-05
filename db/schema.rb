83831c71a3da18aeab240147edcb5a4f52d97d45
Sambath Prum <psb.sambath@gmail.com>
2015-01-05 13:04:17 -0600

    add more columns to website

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141219163612) do
+ActiveRecord::Schema.define(version: 20150105190253) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 20141219163612) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "name"
+    t.string   "description"
+    t.string   "token"
+    t.boolean  "verified"
   end
 
   add_index "websites", ["user_id"], name: "index_websites_on_user_id"
