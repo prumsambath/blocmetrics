b24fea5ff42a622e73c46a6d045927e346ebbd4c
Sambath Prum <psb.sambath@gmail.com>
2015-01-14 11:58:00 -0600

    fix adding user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def not_found
     raise ActionController::RoutingError.new('Not found')
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
   end
 end
