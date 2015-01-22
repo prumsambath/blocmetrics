71686ad55b28af8ae6f1c82678957cfa66fb56c0
Sambath Prum <psb.sambath@gmail.com>
2015-01-22 11:11:18 -0600

    refactor by using context

--- spec/models/website_spec.rb
+++ spec/models/website_spec.rb
@@ -4,29 +4,32 @@ RSpec.describe Website, :type => :model do
   it { should validate_presence_of :name }
   it { should validate_presence_of :address }
 
-  it "should create a website if attributes' value are valid" do
-    website = create(:website)
-    expect(website).to be_valid
+  context 'with valid attributes' do
+    let(:website) { create(:website) }
+
+    it "creates a website" do
+      expect(website).to be_valid
+    end
+
+    it "creates a non-verified website by default" do
+      expect(website.verified).to be_falsey
+    end
+
+    it "generates a token by default" do
+      expect(website.token).to_not be_nil
+    end
+
+    describe "#to_csv" do
+      xit "exports all events to csv successfully" do
+        website = create(:website_with_events)
+      end
+    end
   end
 
-  it "should not create a website if attributes's value are not valid" do
-    website = build(:invalid_website)
-    expect(website).to_not be_valid
-  end
-
-  it "should create a non-verified website by default" do
-    website = create(:website)
-    expect(website.verified).to be_falsey
-  end
-
-  it "should generate a token by default" do
-    website = create(:website)
-    expect(website.token).to_not be_nil
-  end
-
-  describe "#to_csv" do
-    xit "should export all events to csv successfully" do
-      website = create(:website_with_events)
+  context 'with invalid attributes' do
+    it "does not create a website" do
+      website = build(:invalid_website)
+      expect(website).to_not be_valid
     end
   end
 end
